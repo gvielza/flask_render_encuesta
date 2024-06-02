8846c2b21ec2bc8460fe46d53cd966e71c1cc702
Gionnelly Vielza Durán <80008338+gvielza@users.noreply.github.com>
2024-06-02 23:51:40 +0000

    flask en render

---
+++ main.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request
+
+from base_datos.conexion import Conexion
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('formulario.html')
+
+@app.route('/saludo', methods=['POST'])
+def saludo():
+    dni = request.form['dni']
+    usuario=request.form['usuario']
+    contrasenna=request.form['contrasenna']
+    conexion=Conexion("base_datos/usuarios.db")
+    conexion.crear_tabla_cliente()
+    conexion.agregar_cliente(dni,usuario,contrasenna)
+    clientes=conexion.mostrar_clientes()
+    conexion.cerrar_conexion()
+    return render_template('resultados.html', dni=dni, usuario=usuario,clientes=clientes)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
